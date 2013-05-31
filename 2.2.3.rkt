0ff00b9643bbe8514b09dbd4245d538971739615
taekeun yeom <ytkkty@gmail.com>
2013-05-31 09:50:51 +0900

    2.2.2, 2.2.3

---
+++ 2.2.3.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.2.3) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")))))
+;2.2.3 find the area of a triangle 1/2 * base * height
+(define (triangle b h)
+  (* 1/2 (* b h)))
